84e52fc307a7d6725627526e7eb5f41c10fcc4df
Niklas Andersson <nianswe@hotmail.com>
2024-09-27 15:30:15 +0000

    Update app_pages v6

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -6,7 +6,7 @@ from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def page_ml_performance_metrics():
-    version = 'v2'
+    version = 'v6'
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
