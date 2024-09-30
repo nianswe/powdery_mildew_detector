d8aeb6925bacd5ca8704677f58405f5debec448f
Niklas Andersson <nianswe@hotmail.com>
2024-09-30 10:55:41 +0000

    Working model changed inverted result

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -6,9 +6,9 @@ def page_project_hypothesis_body():
     st.write("### Project Hypothesis and Validation")
 
     st.success(
-        f"* We suspect Cherry leaves infected with Powdery Mildew have clear marks/signs, "
-        f"a white faded colour, that can differentiate them from an uninfected leaf. \n\n"
-        f"* An Image Montage shows that typically a leaf infected with powdery mildew has white faded structure. \n"
+        f"* We suspect Cherry leaves infected with Powdery Mildew have clear marks/signs,  "
+        f"infected plants display white powdery spots on the leaves and stems, that can differentiate them from an uninfected leaf. \n\n"
+        f"* An Image Montage shows that typically a leaf infected with powdery mildew has white powdery spots and structure. \n"
         f"Average Image, Variability Image and Difference between Averages studies did not reveal "
         f"any clear pattern to differentiate one from another."
 
